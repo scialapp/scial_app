f1d864e3414380bfbda79cf551629180b6b42c50
Florian Leeser <fleeser@scial.app>
2024-01-06 15:49:49 +0100

    chore: integrate Supabase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:scial_app/env.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'package:scial_app/src/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
+  await Supabase.initialize(
+    url: Env.supabaseUrl,
+    anonKey: Env.supabaseAnonKey
+  );
+
   runApp(const App());
 }
+
+final SupabaseClient client = Supabase.instance.client;
